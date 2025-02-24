57ba7c2fa31237fafde5f01f6e651b8115717415
Rafif Ramdhana <rafiframadhana.cs@gmail.com>
2025-02-24 20:26:05 +0700

    Fix styling

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export default [
             alt: "Mecca"
         },
         title: "Mecca",
-        country: "S A U D I  A R A B I A",
+        country: "SAUDI ARABIA",
         googleMapsLink: "https://maps.app.goo.gl/zwHDQKdxqCmiuYnR8",
         text: "Mecca is Islam's holiest city, as it's the birthplace of the Prophet Muhammad and the site of the Hajj pilgrimage."
     },
@@ -17,7 +17,7 @@ export default [
             alt: "Medina"
         },
         title: "Medina",
-        country: "S A U D I  A R A B I A",
+        country: "SAUDI ARABIA",
         googleMapsLink: "https://maps.app.goo.gl/WFh8voKmw6N6NMqp7",
         text: "Medina is the second holiest city in Islam, home to the Prophet's Mosque, a key pilgrimage site."
     },
@@ -28,7 +28,7 @@ export default [
             alt: "Taj Mahal"
         },
         title: "Taj Mahal",
-        country: "I N D I A",
+        country: "INDIA",
         googleMapsLink: "https://maps.app.goo.gl/oT2hhGBTybYHhYyf7",
         text: "The Taj Mahal is an ivory-white marble mausoleum on the south bank of the Yamuna River in Agra, India."
     },
@@ -39,7 +39,7 @@ export default [
             alt: "Jama Masjid Delhi"
         },
         title: "Jama Masjid",
-        country: "I N D I A",
+        country: "INDIA",
         googleMapsLink: "https://maps.app.goo.gl/TC8uFypae9rtoCpC8",
         text: "Jama Masjid in Delhi is one of the largest mosques in India, built by Mughal Emperor Shah Jahan."
     },
@@ -50,7 +50,7 @@ export default [
             alt: "Agra Fort"
         },
         title: "Agra Fort",
-        country: "I N D I A",
+        country: "INDIA",
         googleMapsLink: "https://maps.app.goo.gl/XCnouFVBNtW3U6656",
         text: "Agra Fort is a historical fort in the city of Agra, a UNESCO World Heritage site and former imperial residence."
     },
@@ -61,7 +61,7 @@ export default [
             alt: "Qutub Minar"
         },
         title: "Qutub Minar",
-        country: "I N D I A",
+        country: "INDIA",
         googleMapsLink: "https://maps.app.goo.gl/5M2PQSwpyp74iqpZ6",
         text: "Qutub Minar is a UNESCO World Heritage Site in Delhi, the tallest brick minaret in the world."
     },
@@ -72,7 +72,7 @@ export default [
             alt: "Hawa Mahal"
         },
         title: "Hawa Mahal",
-        country: "I N D I A",
+        country: "INDIA",
         googleMapsLink: "https://maps.app.goo.gl/y8PAAhyYTr6WpzX26",
         text: "Hawa Mahal, also known as the Palace of Winds, is a stunning architectural masterpiece in Jaipur."
     },
@@ -83,7 +83,7 @@ export default [
             alt: "Amer Fort"
         },
         title: "Amer Fort",
-        country: "I N D I A",
+        country: "INDIA",
         googleMapsLink: "https://maps.app.goo.gl/vusHqZGhCeSm9zNU6",
         text: "Amer Fort is a majestic fort near Jaipur, known for its artistic Hindu-style elements and rich history."
     },
@@ -94,7 +94,7 @@ export default [
             alt: "Nainital"
         },
         title: "Nainital",
-        country: "I N D I A",
+        country: "INDIA",
         googleMapsLink: "https://maps.app.goo.gl/nfa1gG3c1ho5nxJs8",
         text: "Nainital is a famous hill station in India, known for its scenic beauty, pleasant climate, and vibrant local culture."
     },    
@@ -105,7 +105,7 @@ export default [
             alt: "Kashmir"
         },
         title: "Kashmir",
-        country: "I N D I A",
+        country: "INDIA",
         googleMapsLink: "https://maps.app.goo.gl/6bPxaPeityFyi4Bn7",
         text: "Kashmir is known for its stunning natural beauty, pristine lakes, and breathtaking landscapes."
     },
@@ -116,7 +116,7 @@ export default [
             alt: "Varanasi"
         },
         title: "Varanasi",
-        country: "I N D I A",
+        country: "INDIA",
         googleMapsLink: "https://maps.app.goo.gl/Jxgqps4ZCYS9HwMQA",
         text: "Varanasi, the spiritual capital of India, is famous for its ghats, temples, and cultural heritage."
     },
@@ -127,7 +127,7 @@ export default [
             alt: "Singapore Changi Airport"
         },
         title: "Singapore Changi Airport",
-        country: "S I N G A P O R E",
+        country: "SINGAPORE",
         googleMapsLink: "https://maps.app.goo.gl/KVyybBD65fYRNoEWA",
         text: "Changi Airport is one of the world's best airports, known for its luxurious amenities and efficient services."
     }
